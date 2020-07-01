94915838a94edfc5c96ff03cc047f101c47d17c6
kingeta <herbybowden@gmail.com>
2020-07-01 12:11:30 +0100

    Add a bit more data

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 
 //var mydata = [{"foo" : "text", "bar" : 30}, {"foo" : "else", "bar" : 21}];
 
-var mydata = [{"Name": "The Master and Margarita", "Author": "Mikhail Bulgakov", "Genre": "russian", "Date": 2019, "Review" : "Took a while, classic."}];
+var mydata = [{"Name": "The Master and Margarita", "Author": "Mikhail Bulgakov", "Genre": "russian", "Date": 2019, "Review" : "Took a while, classic."},
+                {"Name": "Cryptonomicon", "Author" : "Neal Stephenson", "Genre" : "scifi", "Date" : 2018, "Review" : "Difficult to put down, Yamamoto features."}];
 //Cryptonomicon : Neal Stephenson : scifi : 2018 : Difficult to put down, Yamamoto features.
 //A Canticle for Leibowitz :  : scifi :  : Lots of Latin, often difficult to parse, quite engrossing. Final section perhaps a little less polished than the first two.
 
